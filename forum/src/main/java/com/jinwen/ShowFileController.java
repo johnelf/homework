dd757abd3a1e0862fc12f8aac0862236fe257c80
John <jwzhang@thoughtworks.com>
2012-09-12 22:42:14 +0800

    remove unused method

--- forum/src/main/java/com/jinwen/ShowFileController.java
+++ forum/src/main/java/com/jinwen/ShowFileController.java
@@ -7,44 +7,12 @@ import org.springframework.web.servlet.mvc.Controller;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class ShowFileController implements Controller {
 
     private ArrayList<String> topics = new ArrayList<String>(0);
-
-    protected void readFileByLines(String fileName) {
-        File file = new File(fileName);
-        BufferedReader reader = null;
-
-        try {
-            reader = new BufferedReader(new FileReader(file));
-            String tempString = null;
-            int line = 1;
-
-            tempString = reader.readLine();
-            while (tempString != null) {
-                topics.add(tempString);
-                tempString = reader.readLine();
-                line++;
-            }
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e1) {
-
-                }
-            }
-        }
-    }
 
     public void getTopics(){
         topics = DBOperation.getINSTANCE().ExecuteQuerySQL("select name from article", "name");
